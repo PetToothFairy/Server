a3df14dcd1baf8c491145f80c9da3048a67a079d
kaswhy <sxshx818@naver.com>
2024-11-14 01:46:57 +0900

    fix(initialize): 깃허브 개인 정보 암호화 하는 부분 삭제
    변경 후
    - 해당 사항 반영

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -1,6 +1,6 @@
 # Parameter Store에서 GitHub 자격 증명을 가져오기
-GH_USERNAME=$(aws ssm get-parameter --name "/github/username" --with-decryption --query "Parameter.Value" --output text)
-GH_PAT=$(aws ssm get-parameter --name "/github/token" --with-decryption --query "Parameter.Value" --output text)
+GH_USERNAME=$(aws ssm get-parameter --name "/github/username" --query "Parameter.Value" --output text)
+GH_PAT=$(aws ssm get-parameter --name "/github/token" --query "Parameter.Value" --output text)
 
 # GitHub 자격 증명 파일 설정
 echo "https://${GH_USERNAME}:${GH_PAT}@github.com" > ~/.git-credentials
