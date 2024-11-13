19aad2e5dba09dac02425b6f1e9bfea861590a8c
kaswhy <sxshx818@naver.com>
2024-11-14 01:39:39 +0900

    fix(all): 깃허브 접근 관련 사용자 정보 추가
    변경 후
    - 해당 변경 사항 반영

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -1,2 +1,11 @@
+# Parameter Store에서 GitHub 자격 증명을 가져오기
+GH_USERNAME=$(aws ssm get-parameter --name "/github/username" --with-decryption --query "Parameter.Value" --output text)
+GH_PAT=$(aws ssm get-parameter --name "/github/token" --with-decryption --query "Parameter.Value" --output text)
+
+# GitHub 자격 증명 파일 설정
+echo "https://${GH_USERNAME}:${GH_PAT}@github.com" > ~/.git-credentials
+git config --global credential.helper store
+
+
 cd /home/ubuntu/Server
 git pull origin master
