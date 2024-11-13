6cb4fb93268604358fdc0d50b541ca89a384baa6
kaswhy <sxshx818@naver.com>
2024-11-14 02:46:39 +0900

    fix(initialize): pull만 하도록 변경
    해당 사항 반영

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -1,11 +1,2 @@
-# Parameter Store에서 GitHub 자격 증명을 가져오기
-GH_USERNAME=$(aws ssm get-parameter --name "/github/username" --query "Parameter.Value" --output text | jq -sRr @uri)
-GH_PAT=$(aws ssm get-parameter --name "/github/token" --query "Parameter.Value" --output text | jq -sRr @uri)
-
-# GitHub 자격 증명 파일 설정
-echo "https://${GH_USERNAME}:${GH_PAT}@github.com" > ~/.git-credentials
-git config --global credential.helper store
-
-
 cd /home/ubuntu/Server
 git pull origin master
