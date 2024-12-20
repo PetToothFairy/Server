feb9d8d9d0530152ac4bc48fe200f28a1464cd4a
Lunawood <lunawood119@gmail.com>
2024-12-20 14:50:53 +0900

    fix: fix result spelling

--- docker-test-server/src/main/java/com/example/server/service/ToothService.java
+++ docker-test-server/src/main/java/com/example/server/service/ToothService.java
@@ -24,8 +24,8 @@ public class ToothService {
     private void init() {
         toothPartEngToKor.put("UNDER_FRONT", "아랫쪽 앞니");
         toothPartEngToKor.put("UP_FRONT", "윗쪽 앞니");
-        toothPartEngToKor.put("UNDER_RIGHT_CANINE", "아래쪽 오른쪽 송곳니");
-        toothPartEngToKor.put("UP_RIGHT_CANINE", "위쪽 오른쪽 송곳니");
+        toothPartEngToKor.put("UNDER_RIGHT_CANINE", "아랫쪽 오른쪽 송곳니");
+        toothPartEngToKor.put("UP_RIGHT_CANINE", "윗쪽 오른쪽 송곳니");
         toothPartEngToKor.put("UNDER_RIGHT_MOLAR_OUTSIDE", "아랫쪽 오른쪽 어금니 바깥쪽");
         toothPartEngToKor.put("UP_RIGHT_MOLAR_OUTSIDE", "윗쪽 오른쪽 어금니 바깥쪽");
         toothPartEngToKor.put("UP_LEFT_MOLAR_CHEWING_SIDE", "윗쪽 왼쪽 어금니 씹는쪽");
@@ -45,7 +45,7 @@ public class ToothService {
     public String evaluationPercentValue(Double percent) {
         if (100 == percent)
             return "적절해요.";
-        else if (70 <= percent)
+        else if (60 <= percent)
             return "주의해요.";
         return "미흡해요.";
     }
